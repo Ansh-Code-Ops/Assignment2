c054910a2f835d5a87d6d506dbabcd810299b630
Akashi <Akashi@DESKTOP-E2HLDVR>
2025-02-07 02:17:37 -0500

    Add project files.

---
+++ Assignment2.Tests/ProductTests.cs
@@ -0,0 +1,265 @@
+﻿using NUnit.Framework;
+using Assignment2; // Include the main project namespace
+
+namespace Assignment2.Tests
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        #region ProdID Tests
+
+        [Test]
+        public void TestProdID_ValidValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            var result = product.ProdID;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void TestProdID_MinValue()
+        {
+            // Arrange
+            var product = new Product(5, "Sample Product", 100, 50);
+
+            // Act
+            var result = product.ProdID;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void TestProdID_MaxValue()
+        {
+            // Arrange
+            var product = new Product(50000, "Sample Product", 100, 50);
+
+            // Act
+            var result = product.ProdID;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(50000));
+        }
+
+        #endregion
+
+        #region ProdName Tests
+
+        [Test]
+        public void TestProdName_ValidValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            var result = product.ProdName;
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Sample Product"));
+        }
+
+        [Test]
+        public void TestProdName_EmptyValue()
+        {
+            // Arrange
+            var product = new Product(123, "", 100, 50);
+
+            // Act
+            var result = product.ProdName;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TestProdName_NullValue()
+        {
+            // Arrange
+            var product = new Product(123, null, 100, 50);
+
+            // Act
+            var result = product.ProdName;
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        #endregion
+
+        #region ItemPrice Tests
+
+        [Test]
+        public void TestItemPrice_ValidValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            var result = product.ItemPrice;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void TestItemPrice_MinValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 5, 50);
+
+            // Act
+            var result = product.ItemPrice;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void TestItemPrice_MaxValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 5000, 50);
+
+            // Act
+            var result = product.ItemPrice;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(5000));
+        }
+
+        #endregion
+
+        #region Stock Increase Tests
+
+        [Test]
+        public void TestIncreaseStock_ValidValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            product.IncreaseStock(10);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(60));
+        }
+
+        [Test]
+        public void TestIncreaseStock_MaxValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 500000);
+
+            // Act
+            product.IncreaseStock(100);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(500100)); // Check that stock doesn't exceed max
+        }
+
+        [Test]
+        public void TestIncreaseStock_MinValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 5);
+
+            // Act
+            product.IncreaseStock(10);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(15));
+        }
+
+        #endregion
+
+        #region Stock Decrease Tests
+
+        [Test]
+        public void TestDecreaseStock_ValidValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            product.DecreaseStock(10);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(40));
+        }
+
+        [Test]
+        public void TestDecreaseStock_MinValue()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 5);
+
+            // Act
+            product.DecreaseStock(5);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestDecreaseStock_TooMuch()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            product.DecreaseStock(60); // Attempting to decrease more than the stock
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(50)); // Stock should remain at 50 because we can't go negative
+        }
+
+        [Test]
+        public void TestDecreaseStock_LargeValueBelowZero()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act & Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => product.DecreaseStock(100000));
+            Assert.That(ex.Message, Is.EqualTo("Stock cannot be negative."));
+        }
+
+        #endregion
+
+        #region Stock Edge Case Tests
+
+        [Test]
+        public void TestIncreaseStock_LargeAmount()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 50);
+
+            // Act
+            product.IncreaseStock(500000);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(500050));
+        }
+
+        [Test]
+        public void TestDecreaseStock_LargeAmount()
+        {
+            // Arrange
+            var product = new Product(123, "Sample Product", 100, 500000);
+
+            // Act
+            product.DecreaseStock(100000);
+
+            // Assert
+            Assert.That(product.StockAmount, Is.EqualTo(400000));
+        }
+
+        #endregion
+    }
+}
---
+++ Assignment2/Product.cs
@@ -0,0 +1,44 @@
+﻿public class Product
+{
+    public int ProdID { get; set; }
+    public string ProdName { get; set; }
+    public int ItemPrice { get; set; }
+    public int StockAmount { get; set; }
+
+    public Product(int prodID, string prodName, int itemPrice, int stockAmount)
+    {
+        ProdID = prodID;
+        ProdName = prodName;
+        ItemPrice = itemPrice;
+        StockAmount = stockAmount;
+    }
+
+    public void IncreaseStock(int amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException("amount", "Amount to increase must be positive.");
+        }
+
+        StockAmount += amount;
+    }
+
+    public void DecreaseStock(int amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException("amount", "Amount to decrease must be positive.");
+        }
+
+        // Prevent stock from going negative by limiting the decrease to the current stock
+        if (amount > StockAmount)
+        {
+            // Stock doesn't go below zero, it remains the same
+            StockAmount = 0;
+        }
+        else
+        {
+            StockAmount -= amount;
+        }
+    }
+}
